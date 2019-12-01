fa57bbdf75dcfc93ff3ecb3cc88ff7122302f7d8
Cristian <cristian.flores@alumnos.uta.cl>
2019-12-01 13:40:28 -0300

    db

--- app/bin/configure_db.sh
+++ app/bin/configure_db.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-export PGPASSWORD = 'node_password'
+export PGPASSWORD = 'crsoq_pass'
 
-database = "crsoq_db"
+APP_LIST="postgres curl nodejs"
+PGUSER='crsoq_user'
+PGDB = 'crsoq_db'
 
-echo "Configuring database: $database..."
+# Crea un nuevo usuario
+psql -U postgres -c CREATE USER $PGUSER WITH ENCRYPTED PASSWORD 'example_password';
+psql -U postgres GRANT ALL PRIVILEGES ON DATABASE $PGDB TO $PGUSER;
 
-dropdb -U node_user crsoq_db
-createdb -U node_user crsoq_db
 
-psql -U node_user crsoq_db < ./bin/sql/$database.sql
+echo "Configuring database: $PGDB..."
 
-echo "$database configured..."
+# Crea la db 'crsoq_db'
+dropdb -U $PGUSER $PGDB;
+createdb -U $PGUSER $PGDB;
+
+# Ejecuta las sql del archivo 'database.sql' sobre la db 'crsoq_db'
+psql -U $PGUSER $PGDB < ./bin/sql/$PGDB.sql
+
+echo "$PGDB configured..."
+
+
+####################
+PGPORT=5432
+PGHOST="my.database.domain.com"
+PGUSER="postgres"
+PGDB="mydb"
+createdb -h $PGHOST -p $PGPORT -U $PGUSER $PGDB
